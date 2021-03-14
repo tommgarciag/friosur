c98dac5eed3f144006cbbd0188252e48b39e07a6
Tomas Ignacio Garcia <15tommgarcia@gmail.com>
2021-03-13 21:23:39 -0300

    Programo apertura y cierre de las faqs

--- js/main.js
+++ js/main.js
@@ -42,9 +42,27 @@ for (foto of fotos) {
     $div.classList = 'marcas_item';
     $div.appendChild($img);
     $marcas.appendChild($div);
-
 }
 
+//console.log($marcas);
+
+// FAQS
 
 
-console.log(marcas); 
+const faqs = document.querySelector('.faqs');
+
+
+faqs.addEventListener('click', (e) => {
+    e.preventDefault();
+    //console.log(e.target);
+    if (e.target.classList.contains('faq_question')) {
+        const rta = e.target.children[0];
+        if (rta.classList.contains('open')) {
+            rta.classList.remove('open');
+        } else {
+            rta.classList.add('open');
+        }        
+    }
+});
+
+
